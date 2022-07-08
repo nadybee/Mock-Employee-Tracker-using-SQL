994bfaa531fc8a893327e44d2e2eed468b0cdea8
Nadybee <natalie@yoodlize.com>
2022-07-08 05:55:27 -0600

    update employee question

--- assets/db/seeds.sql
+++ assets/db/seeds.sql
@@ -6,12 +6,12 @@ VALUES ('marketing'),
         ('C-Suite');
 
 
-INSERT INTO roles (job_title, department_name, salary)
-VALUES ('CEO', 'C-Suite', 10000),
-        ('CPO', 'Product', 8000),
-        ('CFO', 'Development', 8000),
-        ('Head of Growth', 'growth', 8000),
-        ('Head of Marketing', 'marketing', 8000);
+INSERT INTO roles (job_title, department_id, salary)
+VALUES ('CEO', 5, 10000),
+        ('CPO', 2, 8000),
+        ('CFO', 3, 8000),
+        ('Head of Growth', 4, 8000),
+        ('Head of Marketing', 1, 8000);
 
 INSERT INTO employees (first_name, last_name, job_title, department_name, salary, manager )        
 
